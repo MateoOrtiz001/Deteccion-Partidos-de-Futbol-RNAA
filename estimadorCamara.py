fd7f370da2d95eafce58d177c5ff701be8181287
MateoOrtiz001 <zitromateo123@gmail.com>
2025-07-12 16:44:48 -0500

    añadiendo movimiento de la cámara

---
+++ estimadorCamara.py
@@ -0,0 +1,90 @@
+import cv2
+import pickle
+import numpy as np
+import os
+import sys
+sys.path.append()
+from utils import measure_distancee
+
+class EstimadorMovimientoCam():
+    def __init__(self,frame):
+        self.lk_params = dict(
+            winSize = (15,15),
+            maxLevel = 2,
+            criteria = (cv2.TERM_CRITERIA_EPS or cv2.TERM_CRITERIA_COUNT, 10, 0.05)
+        )
+        
+        self.minDistance = 5
+        
+        firstFrameGrayScale = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        maskFeatures = np.zeros_like(firstFrameGrayScale)
+        maskFeatures[:,0:20] = 1
+        maskFeatures[:,900:1050] = 1
+        
+        self.features = dict(
+            maxCorners = 100,
+            qualityLevel = 0.3,
+            minDistance = 3,
+            blockSize = 7,
+            mask = maskFeatures
+        )
+    
+    def get_camera_movement(self,frames,readFromStub=False, stubPath=None):
+        
+        if readFromStub and stubPath is not None and os.path.exists(stubPath):
+            with open(stubPath,'rb') as f:
+                return pickle.load(f)
+        
+        cameraMovement = [[0,0]]*len(frames)
+        
+        oldGray = cv2.cvtColor(frames[0],cv2.COLOR_BGR2GRAY)
+        oldFeatures = cv2.goodFeaturesToTrack(oldGray,**self.features)
+        
+        for frameNum in range[1,len(frames)]:
+            frameGray = cv2.cvtColor(frames[frameNum],cv2.COLOR_BGR2GRAY)
+            newFeatures, _,_ = cv2.calcOptilcalFlowPyrLK(oldGray, frameGray, None, **self.lk_params)
+            
+            maxDistance = 0
+            cameraMov_x ,cameraMov_y = 0,0
+            
+            for i, (new,old) in enumerate(zip(oldFeatures,oldFeatures)):
+                newFeatures_point = new.ravel()
+                oldFeatures_point = new.ravel()
+                
+                distance = measure_distance(newFeatures_point,oldFeatures_point)
+                
+                if distance > maxDistance:
+                    maxDistance = distance
+                    cameraMov_x = oldFeatures_point[0] - newFeatures_point[0]
+                    cameraMov_y = oldFeatures_point[1] - newFeatures_point[1]
+                    
+            if maxDistance > self.minDistance:
+                cameraMovement[frameNum] = [cameraMov_x,cameraMov_y]
+                oldFeatures = cv2.goodFeaturesToTrack(frameGray,**self.features)
+                
+            oldGray = frameGray.copy()
+            
+        if stubPath is not None:
+            with open(stubPath,'wb') as f:
+                pickle.dump(cameraMovement,f)
+            
+        return cameraMovement
+    
+    def draw_camera_movement(self, frames, camera_movement_per_frame):
+        output_frames=[]
+        
+        for frameNum, frame in enumerate(frames):
+            frame = frame.copy()
+            
+            overlay = frame.copy()
+            cv2.rectangle(overlay,(0,0),(500,100), (255,255,255), -1)
+            alpha = 0.6
+            cv2.addWeighted(overlay,alpha,frame,1-alpha,0,frame)
+            
+            xMov, yMov = camera_movement_per_frame[frameNum]
+            frame = cv2.putText(frame,f"Movimiento de cámara X: {xMov: 2f}", (10,30), cv2.FONT_HERSHEY_SIMPLEX,1 (0,0,0),3)
+            frame = cv2.putText(frame,f"Movimiento de cámara Y: {yMov: 2f}", (10,60), cv2.FONT_HERSHEY_SIMPLEX,1 (0,0,0),3)
+            
+            output_frames.append(frame)
+            
+            return output_frames
