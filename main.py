c720c47f599fb0246c6fad6465249b64ebfea7cb
Santiago Botero <75955930+SantiagoB07@users.noreply.github.com>
2025-07-14 13:29:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from utils import read_video, save_video
 from trackers import Tracker
 import cv2  
 import numpy as np
-from team_assigner import TeamAssigner
+from assigner import TeamAssigner
 
 
 def main():
